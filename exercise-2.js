44d0eb1ec81bbffd955b1d8fa490d6035bf0d611
Abdulajishs <abdulajeeshsb@gmail.com>
2024-11-18 14:47:55 +0530

    promise exercises

---
+++ exercise-2.js
@@ -0,0 +1,42 @@
+/*
+    1. Print out "Program started" at the start of your code
+    2. Create a Promise that resolves after 3 seconds
+    3. Log out the promise while it's pending
+    4. Print out "Program in progress..." as well
+
+    5. Print out "Step 1 complete" when the first promise fulfills
+    6. Have the first promise return another new Promise that will
+       fulfill after 3 seconds with the message: "Step 2 Complete"
+
+    7. Print out the message from the second promise after it
+       fulfills ("Step 2 Complete")
+
+    HINT: Use setTimeout for the delay
+*/
+
+console.log("Program started");
+
+let first = new Promise((resolve, reject) => {
+   console.log("Promise1 in pending state");
+   
+   setTimeout(() => {
+      resolve("Step 1 complete")
+   }, 3000);
+})
+
+let second = new Promise((resolve, reject) => {
+   console.log("Promise2 in pending state");
+   
+   setTimeout(() => {
+      resolve("Step 2 complete")
+   }, 3000);
+})
+
+console.log("Program in progress...");
+
+first.then((data)=>{
+   console.log(data);
+   return second
+}).then((data)=>{
+   console.log(data)
+})
