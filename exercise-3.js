5fac5ae72ad5b55885d1236f4c90c2b9990d18af
Abdulajishs <abdulajeeshsb@gmail.com>
2024-11-20 11:03:57 +0530

    changes made on exercise-3

--- exercise-3.js
+++ exercise-3.js
@@ -5,15 +5,15 @@
     3. Log out the promise while it's pending
     4. Print out "Program in progress..." as well
 
-    5. Create a first Promise chain using the promise above and
-       Print out the resolved value when the first promise fulfills
-    6. Have this first promise return another new Promise that will
+    5. Create a myPromise Promise chain using the promise above and
+       Print out the resolved value when the myPromise promise fulfills
+    6. Have this myPromise promise return another new Promise that will
        fulfill after 2 seconds with the message:
-       "First promise chain complete!"
+       "myPromise promise chain complete!"
     7. Print out the message from the above promise after it
-       fulfills ("First promise chain complete!")
+       fulfills ("myPromise promise chain complete!")
 
-    8. Create a second Promise chain using the first promise above and
+    8. Create a second Promise chain using the myPromise promise above and
        Print out the resolved value when the second promise fulfills
     9. Have this second promise return another new Promise that will
        fulfill after 10 seconds with the message:
@@ -33,35 +33,40 @@
 
 console.log("Program started");
 
-let first = new Promise((resolve, reject) => {
+let myPromise = new Promise((resolve, reject) => {
    console.log("Promise1 in pending state");
    setTimeout(() => {
       resolve({data: "Hello, friend!", error: null})
    }, 5000);
 })
 
-let second = new Promise((resolve, reject) => {
-   console.log("Promise2 in pending state");
-   setTimeout(() => {
-      resolve("First promise chain complete!")
-   }, 2000);
-})
-
-let third = new Promise((resolve, reject) => {
-   console.log("Promise3 in pending state");
-   setTimeout(() => {
-      resolve("Second promise chain complete!")
-   }, 10000);
-})
-
 console.log("Program in progress...");
 
-first.then((data)=>{
+myPromise.then((data)=>{
    console.log(data);
-   return second
+   return new Promise((resolve, reject) => {
+      console.log("Promise2 in pending state");
+      setTimeout(() => {
+         resolve("first promise chain complete!")
+      }, 2000);
+   })
 }).then((data)=>{
    console.log(data)
-   return third
+}).catch((err)=>{
+   console.log(err)
+})
+
+myPromise.then(()=>{
+   return new Promise((resolve, reject) => {
+      console.log("Promise3 in pending state");
+      setTimeout(() => {
+         resolve("Second promise chain complete!")
+      }, 10000);
+   })
 }).then((data)=>{
-   console.log(data)
+   console.log(data);
+   
+}).catch((err)=>{
+   console.log(err);
+   
 })
